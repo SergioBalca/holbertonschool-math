39a91dbeecd51b768cee69a466375f3f5e72aad4
SergioBalca <3896@holbertonschool.com>
2021-11-08 19:20:23 -0800

    function that displays the complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -16,17 +16,33 @@ void display_complex_number(complex c)
 	}
 	if (!c.re && c.im)
 	{
+		if (c.im == 1)
+		{
+			printf("i\n");
+		}
+		else
 		printf("%0.0fi\n", c.im);
 	}
 	if (c.re && c.im)
 	{	
 		if (c.im > 0)
+		{
+			if (c.im == 1)
+			{
+				printf("%0.0f + i\n", c.re);
+			}
+			else	
 			printf("%0.0f + %0.0fi\n", c.re, c.im);
+		}
 		else
+		{
+			if (c.im == -1) 
+			{
+				printf("%0.0f - i\n", c.re);
+			}
+			else
 			printf("%0.0f - %0.0fi\n", c.re, -c.im);
-	}
-	else
-	{
-		printf("\n");
+
+		}
 	}
 }
