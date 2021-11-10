08f654454fca9d7cac53ccc5b3d9e7e2bd1b808a
SergioBalca <3896@holbertonschool.com>
2021-11-09 18:29:19 -0800

    function that displays the complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,53 +10,33 @@
 
 void display_complex_number(complex c)
 {
+
 	if (c.re && !c.im)
 	{
-		printf("%.1f\n", c.re);
+		printf("%0.0f\n", c.re);
 	}
-	if (!c.re && c.im)
-	{
-		if (c.im > 0)
-		{	
-			if (c.im == 1)
-			{
-				printf("i\n");
-			}
-			else
-				printf("%.1fi\n", c.im);
-		}
-		else
-		{
-			if (c.im == -1)
-			{
-				printf("-i\n");
-			}
-			else
-			{
-				printf("%.1fi\n", c.im);
-			}
-		}	
-	}
-	if (c.re && c.im)
-	{
-		if (c.im > 0)
-		{
-			if (c.im == 1)
-			{
-				printf("%.1f + i\n", c.re);
-			}
-			else
-			printf("%.1f + %.1fi\n", c.re, c.im);
-		}
-		else
-		{
-			if (c.im == -1)
-			{
-				printf("%.1f - i\n", c.re);
-			}
-			else
-			printf("%.1f - %.1fi\n", c.re, -c.im);
 
+	if (c.im)
+	{
+		if (c.im > 0)
+		{
+			if (c.im == 1)
+			
+				printf("%0.0f + i\n", c.re);
+			
+			else
+				printf("%0.0f + %0.0fi\n", c.re, c.im);
+			
+		}
+		else
+		{
+			if (c.im == -1)
+			
+				printf("%0.0f - i\n", c.re);
+			
+			else
+				printf("%0.0f - %0.0fi\n", c.re, -c.im);
+			
 		}
 	}
 }
