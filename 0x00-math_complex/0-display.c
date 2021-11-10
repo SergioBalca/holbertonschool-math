166fb45e1901f5a331c6934d84c739580c8ca38d
SergioBalca <3896@holbertonschool.com>
2021-11-09 19:28:37 -0800

    function that displays the complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -22,20 +22,20 @@ void display_complex_number(complex c)
 		{
 			if (c.im == 1)
 			
-				printf("%f + i\n", c.re);
+				printf("0.0%f + i\n", c.re);
 			
 			else
-				printf("%f + %fi\n", c.re, c.im);
+				printf("%0.0f + %0.0fi\n", c.re, c.im);
 			
 		}
 		else
 		{
 			if (c.im == -1)
 			
-				printf("%f - i\n", c.re);
+				printf("%0.0f - i\n", c.re);
 			
 			else
-				printf("%f - %fi\n", c.re, -c.im);
+				printf("%0.0f - %0.0fi\n", c.re, -c.im);
 			
 		}
 	}
