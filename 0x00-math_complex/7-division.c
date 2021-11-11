34fc3503eea0bc2b557de0ba4d224d7a1a22f1ec
SergioBalca <3896@holbertonschool.com>
2021-11-11 08:42:30 -0800

    function that performs the division operation to complex numbers

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -15,14 +15,17 @@ void division(complex c1, complex c2, complex *c3)
 	conj = conjugate(c2);
 	multiplication(c1, conj, &c4); /* numerator */
 	multiplication(c2, conj, &c5); /* denotinator */
-	
-	if (c5.re != 0)
-	{
-		div.re = c4.re / c5.re;
-		div.im = c4.im / c5.re; /*c5.im = 0*/
 
-		c3->re = div.re;
-		c3->im = div.im;
+	if (c2.re != 0 && c2.im != 0)
+	{
+		if (c5.re != 0)
+		{
+			div.re = c4.re / c5.re;
+			div.im = c4.im / c5.re; /*c5.im = 0*/
+
+			c3->re = div.re;
+			c3->im = div.im;
+		}
 	}
 }
 
